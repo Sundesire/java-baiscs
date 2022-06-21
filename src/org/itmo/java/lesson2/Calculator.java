f7efa1d9f06d14593c0090f710e98a87e2e264aa
Ivan Babushkin <zet6666@gmail.com>
2022-06-21 18:44:03 +0300

    Initial commit

---
+++ src/org/itmo/java/lesson2/Calculator.java
@@ -0,0 +1,29 @@
+package org.itmo.java.lesson2;
+
+public class Calculator {
+    public static void main(String[] args) {
+//        Integer summ = summ(3,5);
+//        Integer extr = extr(4,2);
+//        Integer mult = mult(21,3);
+//        Integer div = div(48,12);
+//        System.out.println("Summ: " + summ + ", extr: " + extr + ", mult: " + mult + ", div: " + div);
+    }
+
+    
+
+    public static int summ(int a, int b) {
+        return a + b;
+    }
+
+    public static int extr (int a, int b) {
+        return a - b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/org/itmo/java/lesson2/Constructor.java
@@ -0,0 +1,7 @@
+package org.itmo.java.lesson2;
+
+public class Constructor {
+    public static void main(String[] args) {
+
+    }
+}
