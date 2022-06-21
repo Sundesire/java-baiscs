6c1517f7c71b731febae9cf601e0d7aae65ee742
Ivan Babushkin <zet6666@gmail.com>
2022-06-21 19:24:36 +0300

    overload methods

--- src/org/itmo/java/lesson2/Calculator.java
+++ src/org/itmo/java/lesson2/Calculator.java
@@ -2,28 +2,61 @@ package org.itmo.java.lesson2;
 
 public class Calculator {
     public static void main(String[] args) {
-//        Integer summ = summ(3,5);
-//        Integer extr = extr(4,2);
-//        Integer mult = mult(21,3);
-//        Integer div = div(48,12);
-//        System.out.println("Summ: " + summ + ", extr: " + extr + ", mult: " + mult + ", div: " + div);
+        int summ = summ(3,5);
+        int extr = extr(4,2);
+        int mult = mult(21,3);
+        int div = div(48, 12);
+        System.out.println("Summ: " + summ + ", extr: " + extr + ", mult: " + mult + ", div: " + div);
     }
 
-
-
+    //Summ
     public static int summ(int a, int b) {
         return a + b;
     }
 
+    public static double summ(double a, double b) {
+        return a + b;
+    }
+
+    public static long summ(long a, long b) {
+        return a + b;
+    }
+
+    //Extraction
     public static int extr (int a, int b) {
         return a - b;
     }
 
+    public static double extr (double a, double b) {
+        return a - b;
+    }
+
+    public static long extr (long a, long b) {
+        return a - b;
+    }
+
+    //Multiply
     public static int mult(int a, int b) {
         return a * b;
     }
 
+    public static double mult(double a, double b) {
+        return a * b;
+    }
+
+    public static long mult(long a, long b) {
+        return a * b;
+    }
+
+    //Divide
     public static int div(int a, int b) {
+        return a / b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+    public static long div(long a, long b) {
         return a / b;
     }
 }
